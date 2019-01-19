808597c55e4d80278c8dbbf663ef99be762be521
David Pomerleau <david.pomerleau.2@etsmtl.net>
2019-01-19 15:06:11 -0500

    Retrait d'une ligne inutile que j'ai oublié de retirer

--- src/main/java/com/ets/gti525/domain/repository/UsersRepository.java
+++ src/main/java/com/ets/gti525/domain/repository/UsersRepository.java
@@ -3,7 +3,6 @@ package com.ets.gti525.domain.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.ets.gti525.domain.entity.Users;
 
@@ -13,7 +12,4 @@ public interface UsersRepository extends JpaRepository<Users, Long> {
 	
 	public List<Users> findByUsername(String username);
 	
-	@Query(nativeQuery = true, value = "SELECT * FROM USERS WHERE ID = 1")
-	public Users getUser();
-
 }
