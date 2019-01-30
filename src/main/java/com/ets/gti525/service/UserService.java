cffee5364f1282f80ae1c4aa334dfd1c47a220c4
Louis-Philippe Bell <louis-philippe.bell.1@ens.etsmtl.ca>
2019-01-29 21:23:53 -0500

    E13 Implemented

--- src/main/java/com/ets/gti525/service/UserService.java
+++ src/main/java/com/ets/gti525/service/UserService.java
@@ -8,20 +8,31 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.ets.gti525.domain.constant.Role;
+import com.ets.gti525.domain.entity.CreditCard;
+import com.ets.gti525.domain.entity.DebitCard;
 import com.ets.gti525.domain.entity.User;
+import com.ets.gti525.domain.repository.CreditCardRepository;
+import com.ets.gti525.domain.repository.DebitCardRepository;
 import com.ets.gti525.domain.repository.UsersRepository;
 import com.ets.gti525.domain.request.CreateUserRequest;
 import com.ets.gti525.domain.response.CreateUserResponse;
 import com.ets.gti525.domain.response.SearchUsersResponse;
 import com.ets.gti525.domain.response.SingleSearchUsers;
+import com.ets.gti525.helper.CardNumberHelper;
 
 @Service
 public class UserService {
 
 	private final UsersRepository usersRepository;
+	private final DebitCardRepository debitRepository;
+	private final CreditCardRepository creditRepository;
 	
-	public UserService(final UsersRepository usersRepository) {
+	public UserService(final UsersRepository usersRepository,
+			final DebitCardRepository debitCardRepository,
+			final CreditCardRepository creditRepository) {
 		this.usersRepository = usersRepository;
+		this.debitRepository = debitCardRepository;
+		this.creditRepository = creditRepository;
 	}
 	
 	public List<User> getUsers() {
@@ -38,9 +49,22 @@ public class UserService {
 
 		User user = new User(username, encodedPassword, Role.USER, 1);
 		
-		usersRepository.save(user);
+		DebitCard dc = new DebitCard();
+		CreditCard cc = new CreditCard();
 		
-		return new CreateUserResponse(HttpStatus.OK, message, username, password);
+		dc.setNbr(Long.parseLong(username));
+		dc.setOwner(user);
+		dc.setBalance(0);
+		
+		String ccNumber = generateCreditCardNumber();
+		cc.setNbr(Long.parseLong(ccNumber));
+		cc.setOwner(user);
+		
+		usersRepository.save(user);
+		debitRepository.save(dc);
+		creditRepository.save(cc);
+		
+		return new CreateUserResponse(HttpStatus.OK, message, username, password, ccNumber);
 	}
 	
 	public SearchUsersResponse searchUsers(String keyword) {
@@ -61,7 +85,19 @@ public class UserService {
 	}
 	
 	private String generateAccountNumber() {
-		return "4540123456780000";
+		String accountNumber = CardNumberHelper.getInstace().generateAccountNumber();
+		while(debitRepository.findByNbr(Long.parseLong(accountNumber)) != null)
+			accountNumber = CardNumberHelper.getInstace().generateAccountNumber();
+		
+		return accountNumber;
+	}
+	
+	private String generateCreditCardNumber() {
+		String accountNumber = CardNumberHelper.getInstace().generateCreditCardNbr();
+		while(creditRepository.findByNbr(Long.parseLong(accountNumber)) != null)
+			accountNumber = CardNumberHelper.getInstace().generateCreditCardNbr();
+		
+		return accountNumber;
 	}
 	
 	private String generatePassword() {
