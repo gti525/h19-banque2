e02f53e1f309dbe4047529eed3b0efe0f6195de7
Forkael <fortin.mlf@gmail.com>
2019-02-05 15:52:15 -0500

    Add card number input and GET API working

--- client/src/LogIn.js
+++ client/src/LogIn.js
@@ -1,7 +1,8 @@
 import * as React from "react";
 import { Card, CardHeader, CardText, CardBody, CardTitle, Input } from 'reactstrap';
 import Button from 'react-bootstrap/lib/Button';
-import { Redirect } from 'react-router-dom'
+import { Redirect } from 'react-router-dom';
+import CreditCardInput from 'react-credit-card-input';
 
 export default class Login extends React.Component { 
   state = {
@@ -19,6 +20,12 @@ export default class Login extends React.Component {
       return <Redirect to='/LoginAdmin' />
     }
   }
+
+  renderRedirectTestAPI = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/DashboardAdmin' />
+    }
+  }
   
   render () {                                   
       return (
@@ -30,53 +37,76 @@ export default class Login extends React.Component {
                     <CardHeader><b>Connexion</b></CardHeader>
                     <CardBody>
                         <CardTitle>Veuillez entrer votre numéro de carte : </CardTitle>
-                        <Input name="numCarte" placeholder="Numéro de carte" />
+                        <CreditCardInput
+                            cardCVCInputRenderer={({ handleCardCVCChange, props }) => (
+                                <input
+                                {...props}
+                                onChange={handleCardCVCChange(e => console.log('cvc change', e))}
+                                />
+                            )}
+                            cardExpiryInputRenderer={({ handleCardExpiryChange, props }) => (
+                                <input
+                                {...props}
+                                onChange={handleCardExpiryChange(e =>
+                                    console.log('expiry change', e)
+                                )}
+                                />
+                            )}
+                            cardNumberInputRenderer={({ handleCardNumberChange, props }) => (
+                                <input
+                                {...props}
+                                onChange={handleCardNumberChange(e =>
+                                    console.log('number change', e)
+                                )}
+                                />
+                            )}
+                            />
                         <br />
                         
-                        <Button bsStyle="success">Entrer</Button>
-                        <Button bsStyle="primary">Test good</Button>
-                        <Button bsStyle="danger">Test bad</Button>
+                        {this.renderRedirectTestAPI()}
+                        <Button bsStyle="success" onClick={this.setRedirect}>Entrer</Button>
                     </CardBody>
                 </Card>
             </div>
+            {/*  
+                <br />
 
-            <br />
+                <div id='questionsContainer'>
+                    <Card className="card">
+                        <CardHeader><b>Connexion</b></CardHeader>
+                            <CardBody>
+                            <CardTitle>Veuillez répondre à la question suivante : </CardTitle>
+                            <CardText>Question ... ... ... </CardText>
+                            <Input name="questionSecrete" placeholder="Réponse" />
+                            <br />
 
-            <div id='questionsContainer'>
-                <Card className="card">
-                    <CardHeader><b>Connexion</b></CardHeader>
-                        <CardBody>
-                        <CardTitle>Veuillez répondre à la question suivante : </CardTitle>
-                        <CardText>Question ... ... ... </CardText>
-                        <Input name="questionSecrete" placeholder="Réponse" />
-                        <br />
+                            <Button bsStyle="success">Entrer</Button>
+                            <Button bsStyle="primary">Test good</Button>
+                            <Button bsStyle="danger">Test bad</Button>
+                        </CardBody>
+                    </Card>
+                </div>
 
-                        <Button bsStyle="success">Entrer</Button>
-                        <Button bsStyle="primary">Test good</Button>
-                        <Button bsStyle="danger">Test bad</Button>
-                    </CardBody>
-                </Card>
-            </div>
+                <br />
 
-            <br />
+                <div id='passwordContainer'>
+                    <Card className="card">
+                        <CardHeader><b>Connexion</b></CardHeader>
+                            <CardBody>
+                            <CardTitle>Veuillez entrer votre mot de passe : </CardTitle>
+                            <Input type="password" name="password" placeholder="" />
+                            <br />
 
-            <div id='passwordContainer'>
-                <Card className="card">
-                    <CardHeader><b>Connexion</b></CardHeader>
-                        <CardBody>
-                        <CardTitle>Veuillez entrer votre mot de passe : </CardTitle>
-                        <Input type="password" name="password" placeholder="" />
-                        <br />
-
-                        <Button bsStyle="success">Entrer</Button>
-                        <Button bsStyle="primary">Test good</Button>
-                        <Button bsStyle="danger">Test bad</Button>
-                    </CardBody>
-                </Card>
-            </div>
+                            <Button bsStyle="success">Entrer</Button>
+                            <Button bsStyle="primary">Test good</Button>
+                            <Button bsStyle="danger">Test bad</Button>
+                        </CardBody>
+                    </Card>
+                </div>
+            */}
 
             {this.renderRedirect()}
-            <Button id="btnAdministration" bsStyle="info" onClick={this.setRedirect}>Administration</Button>
+            <Button id="btnAdministration" bsStyle="info" onClick={ this.setRedirect}>Administration</Button>
         </div>
       )
    }
--- client/src/Views/DashboardAdmin.js
+++ client/src/Views/DashboardAdmin.js
@@ -2,52 +2,39 @@ import * as React from "react";
 
 export default class DashbordAdmin extends React.Component {
    state = {
-      creditCards: [],
+      debitCards: [],
       error: null,
       isLoading: true
    }
 
-   fetchUsers() {
-      fetch('http://localhost:3000/api/v1/account/debitCard/22212345')
-        .then(response => response.json())
-        .then(data =>
-          this.setState({
-            creditCards: data.map((x) => ({
-               balance: x.balance,
-               nbr: x.nbr
-             })),
+   fetchDebitCards() {
+      fetch("http://localhost:3000/api/v1/account/debitCard/22212345")
+         .then(response => response.json())
+         .then(data => this.setState({
+            debitCards: data,
             isLoading: false,
-          })
-        )
+         }))
         .catch(error => this.setState({ error, isLoading: false }));
-    }
+   }
 
    componentDidMount() {
-      this.fetchUsers();
+      this.fetchDebitCards();
    }
    
    render () {
-      const { isLoading, creditCards, error } = this.state;
-
+      const { isLoading, debitCards, error } = this.state;
       return (
          <React.Fragment>
-            <h1>Carte</h1>
+            <h1>Données API test : </h1>
+            
             {error ? <p>{error.message}</p> : null}
+          
             {!isLoading ? (
-            creditCards.map(creditCard => {
-               const { nbr, balance, cardLimit, monthExp, yearExp } = creditCard;
-               return (
-                  <div>
-                  <p>Numéro de carte: {nbr}</p>
-                  <p>Balance: {balance}</p>
-                  <hr />
-                  </div>
-               );
-            })
-            ) : (
-            <h3>Loading...</h3>
+               <p>Numéro : {this.state.debitCards.nbr} <br /> Balance : {this.state.debitCards.balance}</p>
+            ):(
+               <h3>Loading...</h3>
             )}
          </React.Fragment>
-      )
-   }
+      );
+    }
 }
