e1046923f37b1787e411b7e2b91d62a0f8eec881
bourgaultp <pascal.bourgault15@gmail.com>
2019-02-13 14:54:55 -0500

    Added header class documentation for repositories.

--- src/main/java/com/ets/gti525/domain/repository/CreditCardTransactionRepository.java
+++ src/main/java/com/ets/gti525/domain/repository/CreditCardTransactionRepository.java
@@ -7,6 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 
 import com.ets.gti525.domain.entity.CreditCardTransaction;
 
+/**
+ * Description : JPA repository for credit card transactions in the database.
+ * 
+ * Course : GTI525-01
+ * Semester : Winter 2019
+ * @author Team bank #2
+ * @version 1.0
+ * @since 20-01-2019
+ */
 public interface CreditCardTransactionRepository extends JpaRepository<CreditCardTransaction, Integer> {
 	@Query(value = "SELECT * FROM CREDIT_CARD_TRANSACTION t WHERE t.CREDIT_CARD_NBR = ?1", nativeQuery = true)
 	public List<CreditCardTransaction> findByCreditCardNbr(long creditCardNbr);
--- src/main/java/com/ets/gti525/domain/repository/PartnerBankRepository.java
+++ src/main/java/com/ets/gti525/domain/repository/PartnerBankRepository.java
@@ -4,6 +4,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ets.gti525.domain.entity.PartnerBank;
 
+/**
+ * Description : JPA repository for partner banks in the database.
+ * 
+ * Course : GTI525-01
+ * Semester : Winter 2019
+ * @author Team bank #2
+ * @version 1.0
+ * @since 09-02-2019
+ */
 public interface PartnerBankRepository extends JpaRepository<PartnerBank, Integer>{
 
 	PartnerBank findByApiKey(String apiKey);
