8d95ce037be0ed8a30f1681212861441efe62978
kian1111 <am45650@ens.etsmtl.ca>
2019-03-20 17:21:53 -0400

    Add footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -56,7 +56,46 @@ class App extends React.Component {
             <Route path="/DetailsClient" render={props=><DetailsClient {...props} state={this.state}/>} />
           </div>
         </BrowserRouter>
-        <div id="horizontal-analytic-banner"></div>
+       
+         
+<footer className="footer">
+
+<div className="container bottom_border">
+<div className="row">
+<div className=" col-sm-4 col-md col-sm-4  col-12 col">
+<h5 className="headin5_amrc col_white_amrc pt2"> DETAIL SUR DESPÉPINIÈRES </h5>
+
+<p className="mb10">Vous pouvez venir à nos bureau ouvert du lundi au vendredi de 8h30am à 17h.</p>
+<p><i className="fa fa-location-arrow"></i> Situé au 8080 localhost </p>
+<p><i className="fa fa-phone"></i>  +91-9999878398  </p>
+<p><i className="fa fa fa-envelope"></i> Contacter nous via courriel : support@Despépinières.com  </p>
+</div>
+<div className=" col-sm-4 col-md  col-6 col">
+<h5 className="headin5_amrc col_white_amrc pt2">À PROPOS DE NOUS</h5>
+
+<ul className="footer_ul_amrc">
+<li>Despépinières est une banque qui fait affaire avec des clients au niveau mondial. On possède des branches principalement aux États-Unis, en Europe et en Asie.</li>
+</ul>
+</div>
+<div className=" col-sm-4 col-md  col-6 col">
+<h5 className="headin5_amrc col_white_amrc pt2">NOUVELLES DES DESPÉPINIÈRES</h5>
+
+<ul className="footer_ul_amrc">
+<li>Venez profitez de notre promotions! Faites trois transactions et payer les frais pour qu'une seule! Ce forfait sera valide jusqu'au 5 Avril 2019</li>
+
+</ul>
+<ul className="footer_ul_amrc">
+<div id="horizontal-analytic-banner"></div>
+</ul>
+</div>
+</div>
+</div>
+<div className="container">
+
+<p className="text-center">© 2019, http://localhost:8080/, Mouvement Despépinières, Tous droits réservé. Despépinières</p>
+</div>
+
+</footer>
       </div>
     );
   }
