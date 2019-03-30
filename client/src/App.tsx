79e5761749528c14b9f69e5e16e408942bc7fe2f
kian1111 <am45650@ens.etsmtl.ca>
2019-03-30 14:52:52 -0400

    Modification Footer

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,6 +12,7 @@ import PaymentCarte from "./components/payment/PaymentCarte";
 import VirementInterac from "./components/virement/VirementInterac";
 import NouveauCompte from "./components/admin/NouveauCompte";
 import DetailsClient from "./components/admin/details/DetailsClient";
+//import { CardFooter } from 'reactstrap';
 
 import "./styles/App.css";
 import logo from "./Images/Logo_banque2.png";
@@ -55,49 +56,41 @@ class App extends React.Component {
             <Route path="/NouveauCompte" render={props=><NouveauCompte {...props} state={this.state}/>} />
             <Route path="/DetailsClient" render={props=><DetailsClient {...props} state={this.state}/>} />
           </div>
+          
         </BrowserRouter>
-       
+        
+    <footer className="App-footer">
+      <div className="container">
+         <div className="row">
+            <div className="col-md-4">                  
+               <h4 className="footertext">À propos de nous</h4>
+                <p>Nous avons des clients au niveau mondiale. Notre headquarter est situé à Montréal</p>
+                
+            </div>
+              <div className="col-md-4">
+
+                  <h4 className="footertext">Détails sur Despépinères</h4>
+                  <p>Situé au localhost:8080 </p>
+                  <p> Numéro de téléphone : 514-555-5555</p>
+               
+              </div>
+              
+              </div>
+              <div className="row">
+            <div id="horizontal-analytic-banner"></div>
+              </div>
+      </div>
+          
          
-<footer className="footer">
+      </footer>
+         
+        
+         
 
-<div className="bottom_border">
-<div className="row footerRow" >
-<div id="horizontal-analytic-banner"></div>
-<div className=" col-sm-4 col-md col-sm-4  col-12 col">
-<h5 className="headin5_amrc col_white_amrc pt2"> DETAIL SUR DESPÉPINIÈRES </h5>
-
-<p className="mb10">Vous pouvez venir à nos bureau ouvert du lundi au vendredi de 8h30am à 17h.</p>
-<p><i className="fa fa-location-arrow"></i> Situé au 8080 localhost </p>
-<p><i className="fa fa-phone"></i>  +91-9999878398  </p>
-<p><i className="fa fa fa-envelope"></i> Contacter nous via courriel : support@Despépinières.com  </p>
-</div>
-<div className=" col-sm-4 col-md  col-6 col">
-<h5 className="headin5_amrc col_white_amrc pt2">À PROPOS DE NOUS</h5>
-
-<ul className="footer_ul_amrc">
-<li>Despépinières est une banque qui fait affaire avec des clients au niveau mondial. On possède des branches principalement aux États-Unis, en Europe et en Asie.</li>
-</ul>
-</div>
-<div className=" col-sm-4 col-md  col-6 col a">
-<h5 className="headin5_amrc col_white_amrc pt2">NOUVELLES DES DESPÉPINIÈRES</h5>
-
-<ul className="footer_ul_amrc">
-<li>Venez profitez de notre promotions! Faites trois transactions et payer les frais pour qu'une seule! Ce forfait sera valide jusqu'au 5 Avril 2019</li>
-
-</ul>
-<ul className="footer_ul_amrc">
-</ul>
-</div>
-</div>
-</div>
-<div className="container">
-
-<p className="text-center">© 2019, http://localhost:8080/, Mouvement Despépinières, Tous droits réservé. Despépinières</p>
-</div>
-
-</footer>
+      
       </div>
     );
+    
   }
 }
 
